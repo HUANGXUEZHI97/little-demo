2d24866c98e8a115305047d2768b9c14935cc025
HUANGXUEZHI97 <804212715@qq.com>
2021-03-14 12:04:41 +0800

    feat：增加mathjs解决精度丢失问题

--- interview-text/src/main.js
+++ interview-text/src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { create, all } from 'mathjs'
+const config = {
+  number:'BigNumber',
+  precision:20
+}
+const math = create(all,config)
 
+Vue.prototype.$math = math
 Vue.config.productionTip = false
 
 new Vue({
